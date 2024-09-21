28800e3367259eb5cfb165625785477c049a8c16
Giuseppe86-lab <sinatragi@gmail.com>
2024-09-21 15:19:11 +0200

    First class completed teacher schedule table but still without constrains

--- classes_sin.py
+++ classes_sin.py
@@ -9,6 +9,7 @@ TABELLA_ORARIO={'1LS':{'LUN':[i for i in range(0,6)],
                        'GIO': [i for i in range(0,6)],
                        'VEN':[i for i in range(0,6)]}}
 
+
 class Docente:
     def __init__(self, cognome, nome, materia, max_ore, classi):
         self.cognome = cognome
@@ -17,6 +18,11 @@ class Docente:
         self.max_ore = max_ore
         self.classi = classi
         self.ore_associate = 0
+        self.orario_settimana = {'LUN':[" " for i in range(0,6)],
+                  'MAR':[" " for i in range(0,9)],
+                  'MER': [" " for i in range(0,6)],
+                  'GIO': [" " for i in range(0,6)],
+                  'VEN':[" " for i in range(0,9)]}
 
 class Classe:
     def __init__(self, nome, docenti_assegnati, tot_ore):
@@ -34,13 +40,16 @@ class Orario:
 
     def genera_orario(self):
         for classe in self.classi:
-            print(classe.nome)
-            for docente, ore in classe.docenti_assegnati.items():
-                print(docente, ore)
+            #print(classe.nome)
+            for ln_docente, ore in classe.docenti_assegnati.items():
+                #print(docente, ore)
                 for _ in range(ore):
-                    print(_)
-                    slot = self.trova_slot_libero(classe)
-                    self.orario_generato[(classe.nome, slot)] = (docente)
+                    #print(_)
+                    slot, (day, hour) = self.trova_slot_libero(classe)
+                    self.orario_generato[(classe.nome, slot)] = (ln_docente)
+                    for teacher in self.docenti:
+                        if teacher.cognome == ln_docente:
+                            teacher.orario_settimana[day][hour] = classe.nome
                     #print(self.orario_generato[(classe.nome, slot)])
                     #print(docente.ore_associate)
                     #docente.ore_associate += 1
@@ -62,11 +71,11 @@ class Orario:
                 #print(ora, 'Lun')
                 if self.slot_orari[classe.nome]['Lun'][ora_lun] == -1:
                     self.slot_orari[classe.nome]['Lun'][ora_lun] = 0
-                    print(f"Lunedì: {ora}")
-                    return ora
+                    #print(f"Lunedì: {ora}")
+                    return ora, ('LUN', ora_lun)
                 else:
-                    print(f"Lunedì - Ora occupata: {ora}")
-                    print(self.slot_orari[classe.nome]['Lun'][ora_lun])
+                    #print(f"Lunedì - Ora occupata: {ora}")
+                    #print(self.slot_orari[classe.nome]['Lun'][ora_lun])
                     continue
 
             elif ore_lun < ora <= (ore_lun+ore_mar):
@@ -74,11 +83,11 @@ class Orario:
                 #print(ora, 'Mar')
                 if self.slot_orari[classe.nome]['Mar'][ora_mar] == -1:
                     self.slot_orari[classe.nome]['Mar'][ora_mar] = 0
-                    print(f"Martedì: {ora}")
-                    return ora
+                    #print(f"Martedì: {ora}")
+                    return ora, ('MAR', ora_mar)
                 else:
-                    print(f"Martedì - Ora occupata: {ora}")
-                    print(self.slot_orari[classe.nome]['Mar'][ora_mar])
+                    #print(f"Martedì - Ora occupata: {ora}")
+                    #print(self.slot_orari[classe.nome]['Mar'][ora_mar])
                     continue
 
             elif (ore_lun + ore_mar) < ora <= (ore_lun+ore_mar+ore_mer):
@@ -86,11 +95,11 @@ class Orario:
                 #print(ora, 'Mer')
                 if self.slot_orari[classe.nome]['Mer'][ora_mer] == -1:
                     self.slot_orari[classe.nome]['Mer'][ora_mer] = 0
-                    print(f"Mercoledì: {ora}")
-                    return ora
+                    #print(f"Mercoledì: {ora}")
+                    return ora, ('MER', ora_mer)
                 else:
-                    print(f"Mercoledì - Ora occupata: {ora}")
-                    print(self.slot_orari[classe.nome]['Mer'][ora_mer])
+                    #print(f"Mercoledì - Ora occupata: {ora}")
+                    #print(self.slot_orari[classe.nome]['Mer'][ora_mer])
                     continue
 
             elif (ore_lun+ore_mar+ore_mer) < ora <= (ore_lun+ore_mar+ore_mer+ore_gio):
@@ -98,11 +107,11 @@ class Orario:
                 #print(ora, 'Gio')
                 if self.slot_orari[classe.nome]['Gio'][ora_gio] == -1:
                     self.slot_orari[classe.nome]['Gio'][ora_gio] = 0
-                    print(f"Giovedì: {ora}")
-                    return ora
+                    #print(f"Giovedì: {ora}")
+                    return ora, ('GIO', ora_gio)
                 else:
-                    print(f"Giovedì - Ora occupata: {ora}")
-                    print(self.slot_orari[classe.nome]['Gio'][ora_gio])
+                    #print(f"Giovedì - Ora occupata: {ora}")
+                    #print(self.slot_orari[classe.nome]['Gio'][ora_gio])
                     continue
 
             elif (ore_lun+ore_mar+ore_mer+ore_gio) < ora <= (ore_lun+ore_mar+ore_mer+ore_gio+ore_ven):
@@ -110,17 +119,15 @@ class Orario:
                 #print(ora, 'Ven')
                 if self.slot_orari[classe.nome]['Ven'][ora_ven] == -1:
                     self.slot_orari[classe.nome]['Ven'][ora_ven] = 0
-                    print(f"Venerdì: {ora}")
-                    return ora
+                    #print(f"Venerdì: {ora}")
+                    return ora, ('VEN', ora_ven)
                 else:
-                    print(f"Venerdì - Ora occupata: {ora}")
-                    print(self.slot_orari[classe.nome]['Ven'][ora_ven])
+                    #print(f"Venerdì - Ora occupata: {ora}")
+                    #print(self.slot_orari[classe.nome]['Ven'][ora_ven])
                     continue
             else:
                 print(ora, "Not valid number")
                 break
-
-
 
 
     def verifica_vincoli(self):
@@ -134,23 +141,46 @@ class Orario:
         nday_to_wday={0: 'LUN', 1: 'MAR', 2: 'MER', 3: 'GIO', 4: 'VEN'}
         for chiave, valore in self.orario_generato.items():
             classe, slot = chiave
-            docente = valore
+            ln_docente = valore
             slot -=1
             if classe == list_courses[0]:
                 nday = slot//6
                 hour = slot%6
                 wday=nday_to_wday[nday]
-                TABELLA_ORARIO[classe][wday][hour]=docente
+                TABELLA_ORARIO[classe][wday][hour]=ln_docente
+
+        with open('output_orario/orario.csv', 'w') as file:
+            for pr_class in TABELLA_ORARIO.keys():
+                file.write(pr_class + '\n')
+                for pr_day in TABELLA_ORARIO[pr_class].keys():
+                    line = str(pr_day + ":")
+                    file.write(line)
+                    for item in TABELLA_ORARIO[pr_class][pr_day]:
+                        file.writelines(item+" ")
+                    file.write('\n')
+
+            for teacher in self.docenti:
+                file.write(teacher.cognome + ": ")
+                for pr_day in teacher.orario_settimana.keys():
+                    line = str(pr_day + ":")
+                    file.write(line)
+                    for item in teacher.orario_settimana[pr_day]:
+                        file.write(item + " ")
+                file.write('\n')
+                #line_teacher=str(teacher.orario_settimana) + "\n"
+                #file.write(line_teacher)
+        """   
         for pr_class in TABELLA_ORARIO.keys():
             print(pr_class)
+            
             for pr_day in TABELLA_ORARIO[pr_class].keys():
-                print(pr_day+":",end="")
+                print(pr_day+":", end="")
                 print(TABELLA_ORARIO[pr_class][pr_day])
-
+                    """
             #if classe==TABELLA_ORARIO.keys
             #print(f"Classe {classe} - Slot {slot}: {docente}")
 
-def get_info(filepath='teachers.csv'):
+def get_info(filepath='input_orario/teachers.csv'):
     """ Read a text file and return the list of
     all the teachers.
     """
